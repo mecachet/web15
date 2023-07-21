ecebe224860c75798542b7acd64b98604b45b37b
mecachet <otoqvilitaia@gmail.com>
2023-07-21 22:37:02 +0400

    made adjustments

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ console.log("Using for loop:");
 for (let i = 0; i <= 50; i++) {
   console.log(i);
 }
-console.log("Using while loop:");
+console.log("Task");
 let i = 0;
 while (i <= 20) {
   console.log(i);
   i++;
 }
-console.log("Using do-while loop:");
+console.log("Task");
 let j = 0;
 do {
   console.log(j);
   j++;
 } while (j <= 30);
 const numbers = [1, 2, 5, 7, 8, 10, 23, 24, 26];
-console.log("Even numbers in the array:");
+console.log("ლუწი რიცხვები");
 for (let k = 0; k < numbers.length; k++) {
   if (numbers[k] % 2 === 0) {
     console.log(numbers[k]);
